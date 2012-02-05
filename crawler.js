199e866de7fb05c2912036e5affca04ac31b29c2
Victor Guerra <vguerra@gmail.com>
2012-02-06 00:10:04 +0100

    crawler learns to save html fetched from url into mongodb.

--- crawler.js
+++ crawler.js
@@ -1,8 +1,30 @@
 var jsdom = require('jsdom');
-var Db = require('mongodb').Db,
-  Connection = require('mongodb').Connection,
-  Server = require('mongodb').Server;
+var Db = require('mongodb').Db;
+var Connection = require('mongodb').Connection;
+var Server = require('mongodb').Server;
+var util = require('util');
 
+var host = process.env['MONGO_NODE_DRIVER_HOST'] != null ? process.env['MONGO_NODE_DRIVER_HOST'] : 'localhost';
+var port = process.env['MONGO_NODE_DRIVER_PORT'] != null ? process.env['MONGO_NODE_DRIVER_PORT'] : Connection.DEFAULT_PORT;
+
+console.log(process.version);
+
+var db = new Db('crawler', new Server(host, port, {}), {native_parser: false, auto_reconnect: true});
+
+var open = function onOpen(error, db) {
+    console.log('Connecting to mongodb: ' + db.databaseName);
+};
+
+function pushToMongo(html) {
+    db.collection('history', function withCollection(error, collection) {
+        collection.insert({'html': html }, function whenDone(error, docs) {
+            console.log('the docs are: ' + util.inspect(docs));
+            docs.forEach(function(doc) {
+                console.dir(doc);
+            });
+        });
+    });  
+};
 
 function start() {
   console.log('starting...');
@@ -10,17 +32,24 @@ function start() {
     html: 'http://www.html5rocks.com/en/',
     scripts: ['http://code.jquery.com/jquery.min.js'], 
     done: function parse(errors, window) {
-      window.$('a').each(function(index) {        
-        console.log(window.$(this).attr('href'));
-        //debugger;
-      });
-    }
+        var html = window.$('html');
+        var attrs = html[0].attributes;  
+        var result = '<html';
+        window.$.each(attrs, function() { 
+            result += ' ' + this.name + '="' + this.value + '"';
+        });    
+        result += '>' + html.html() + '</html>';
+        console.log(result);
+        pushToMongo(result);
+      }
   });
 }
 
 process.on('uncaughtException', function (err) {
   console.log('Caught exception: ' + err);
 });
+
+db.open(open);
 
 start();
 //setInterval(start, 5000);
