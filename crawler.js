9aa1fdb8020f64536629b42e8113e54668ba65fd
Victor Guerra <vguerra@gmail.com>
2012-02-05 02:16:55 +0100

    First steps: playing around with modules jsdom and mongodb.

--- crawler.js
+++ crawler.js
@@ -1,1 +1,26 @@
-var htmlParser = require('html5');
+var jsdom = require('jsdom');
+var Db = require('mongodb').Db,
+  Connection = require('mongodb').Connection,
+  Server = require('mongodb').Server;
+
+
+function start() {
+  console.log('starting...');
+  jsdom.env({
+    html: 'http://www.html5rocks.com/en/',
+    scripts: ['http://code.jquery.com/jquery.min.js'], 
+    done: function parse(errors, window) {
+      window.$('a').each(function(index) {        
+        console.log(window.$(this).attr('href'));
+        //debugger;
+      });
+    }
+  });
+}
+
+process.on('uncaughtException', function (err) {
+  console.log('Caught exception: ' + err);
+});
+
+start();
+//setInterval(start, 5000);
